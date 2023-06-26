5a9fee6ebfc8ac46228ce01a175ed043f7b8191d
eldarlrd <eldarlrd@pm.me>
2023-06-26 17:00:53 +0400

    init: Setup ArrowJS & TailwindCSS

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+}
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import './style.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+`;
