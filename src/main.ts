bcb867775ecdef4c06451923b93fad906a9ec657
eldarlrd <eldarlrd@pm.me>
2023-07-01 21:30:35 +0400

    chore: Add PostCSS

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+  <h1 class='text-3xl font-bold underline'>Post CSS</h1>
 `;
