030466c3c4f05f6f3f109bf86e651be17c4bf1bf
eldarlrd <eldarlrd@pm.me>
2023-07-03 16:20:34 +0400

    feat: Functional plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
-    'postcss-import': {},
+    'postcss-advanced-variables': {},
     'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
-    'postcss-mixins': {},
     'postcss-extend': {},
-    'postcss-simple-vars': {},
     'postcss-preset-env': {
       features: { 'nesting-rules': false }
     },
