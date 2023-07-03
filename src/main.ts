2165c1b606325892542cfce5b96fb1a7ff4e7649
eldarlrd <eldarlrd@pm.me>
2023-07-04 01:21:33 +0400

    feat: Stylelint

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './main.pcss';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1 class='text-3xl font-bold underline'>Hello world!</h1>
-`;
+const appContent = document.getElementById('app') as HTMLDivElement;
+if (appContent !== null)
+  appContent.innerHTML = `
+    <h1 class='font-main text-3xl font-bold underline'>Hello world!</h1>
+  `;
