7d32ad1fd7886d3f13be18346128a4c723b788fe
eldarlrd <eldarlrd@pm.me>
2023-07-07 19:40:44 +0400

    feat: Header & Footer

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
 import { t, r, w } from '@arrow-js/core';
+import githubLogo from './assets/github.svg';
 import './main.pcss';
 
-const appElement = document.getElementById('app');
+const githubURL = 'https://github.com/eldarlrd';
+
 const template = t`
-  <h1 class="font-main text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <header>
+    <h1 class='font-main drop-shadow-xl select-none m-4 text-white text-center text-3xl font-bold'>
+      Fifteen Puzzle
+    </h1>
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer>
+    <p class='flex items-center justify-center flex-col font-main text-center drop-shadow-xl select-none m-4 text-white font-bold text-lg'>
+      by <a
+           class='flex gap-1 items-center justify-center text-center hover:scale-110'
+           target='_blank'
+           rel='noreferrer'
+           href=${githubURL}>
+           <img
+             class='w-6 h-6'
+             alt='GitHub Logo'
+             src=${githubLogo}
+           > eldarlrd
+         </a>
+    </p>
+  </footer>
 `;
 
+const appElement = document.getElementById('app');
 if (appElement !== null) template(appElement);
